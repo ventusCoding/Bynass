63ab22d0707ec6d5ea33aa0f3acb1db88887501f
anwer <anwer.baccar2@gmail.com>
2021-07-14 12:30:27 +0100

    add login page xml

---
+++ app/src/main/java/com/example/bynass/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.bynass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+    }
+}
